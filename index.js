4408c0a4c53af72f5389eee52d1f9ae2890aa2c3
凡凡 <fanfan@fanfandeMacBook-Pro-2.local>
2024-04-07 19:11:24 +0800

    fix: 提交代码测试用

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: JYunProject
+ * @version: 1.0
+ * @Author: 刘中保
+ * @Date: 2024-04-07 19:10:36
+ * @LastEditors: 刘中保
+ * @LastEditTime: 2024-04-07 19:10:48
+ */
+console.log('提交代码测试使用')
